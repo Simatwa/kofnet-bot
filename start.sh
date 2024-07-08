adab571bc97b3621fbf47ba4826fd72c64d7c472
smartwa <smartwacaleb@gmail.com>
2024-07-08 20:31:03 +0300

    feat: Add kofnet dependency and remove test script

--- start.sh
+++ start.sh
@@ -9,5 +9,6 @@ git clone https://github.com/Simatwa/kofnet-bot.git
 cp .env $DIR/
 cd $DIR
 pip install -U pip
+pip install git+https://github.com/Simatwa/kofnet.git
 pip install -r requirements.txt
 python run.py
