527b8318682f8674accf64d1d19c813a7f1aaf03
smartwa <smartwacaleb@gmail.com>
2024-07-08 20:24:40 +0300

    feat: Minor changes

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+DIR="kofnet-bot"
+
+if [[ -d "$DIR" ]]; then
+   rm $DIR -rf
+fi
+
+git clone https://github.com/Simatwa/kofnet-bot.git
+cp .env $DIR/
+cd $DIR
+pip install -U pip
+pip install -r requirements.txt
+python run.py
