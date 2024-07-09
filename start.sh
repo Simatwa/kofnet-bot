571812c3a756a01496f6c8382a72245479b041c0
smartwa <smartwacaleb@gmail.com>
2024-07-09 14:18:19 +0300

    feat: Update dependencies and scripts

--- start.sh
+++ start.sh
@@ -9,6 +9,5 @@ git clone https://github.com/Simatwa/kofnet-bot.git
 cp .env $DIR/
 cd $DIR
 pip install -U pip
-pip install git+https://github.com/Simatwa/kofnet.git
 pip install -r requirements.txt
 python run.py
